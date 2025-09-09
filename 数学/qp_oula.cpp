61e0de79447d7a58b884aaaf595576f2f85253b1
HTyin12 <2546769684@qq.com>
2025-09-09 12:35:30 +0800

    格式修改

--- 数学/qp_oula.cpp
+++ 数学/qp_oula.cpp
@@ -1,8 +1,8 @@
 int qp_oula(string &s){
-        if(s.size()>=10||stoi(s)>=P-1){
-            int m=0;for(auto i:s)m=(m*10+i-'0')%(P-1);
-            return m+P-1;
-        }else{
-            return stoi(s);
-        }
+    if(s.size()>=10||stoi(s)>=P-1){
+        int m=0;for(auto i:s)m=(m*10+i-'0')%(P-1);
+        return m+P-1;
+    }else{
+        return stoi(s);
     }
+}
